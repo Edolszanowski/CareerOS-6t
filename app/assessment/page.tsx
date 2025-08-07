066a005d5b42f6c49f3800cfad596c951a4a2ffa
Ed Oly <>
2025-08-07 10:00:38 -0500

    Assessment view
    Trying to update page.tsx so that it shows the proper and most updated code from vercel.  It continues to show older screens.  We have decided to abandon this and just use vercel reviews.

--- app/assessment/page.tsx
+++ app/assessment/page.tsx
@@ -5,16 +5,8 @@ import { OnboardingProvider } from "@/contexts/onboarding-context"
 
 export default function AssessmentPage() {
   return (
-    <div className="min-h-screen">
-      <OnboardingProvider>
-        {/* Create a wrapper that sets the required context */}
-        <AssessmentWrapper />
-      </OnboardingProvider>
-    </div>
+    <OnboardingProvider>
+      <AIAssessmentFlow />
+    </OnboardingProvider>
   )
-}
-
-function AssessmentWrapper() {
-  // This ensures the OnboardingProvider has the required data
-  return <AIAssessmentFlow />
 }
